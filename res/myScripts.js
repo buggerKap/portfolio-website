897096a9a67fe711f2992b94018b8a4a96313573
Tylerbrass <222108399+buggerKap@users.noreply.github.com>
2026-04-24 11:49:15 +0100

    finished

--- res/myScripts.js
+++ res/myScripts.js
@@ -1,5 +1,26 @@
-function myFunction(){
-    
+let slideIndex = 1;
+ShowSlide("image",slideIndex);
+ShowSlide("image2",slideIndex);
 
-    
+//slide an element (video, image) for the project showcase
+//display current slide
+function ShowSlide(j, x){
+    let i;
+    let slides = document.getElementsByClassName(j);
+    if(x > slides.length) {slideIndex= 1}
+    if(x < 1){ slideIndex = slides.length};
+
+    for(i = 0; i < slides.length; i++){
+        slides[i].style.display = "none";
+    }
+    slides[slideIndex-1].style.display = "block";
+
+}
+
+function plusSlides(j, n){
+    ShowSlide(j,slideIndex += n);
+}
+
+function CurrentSlide(n){
+    ShowSlide(slideIndex = n);
 }
